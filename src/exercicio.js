a4f55decd00fd3b3708442d4f81e22f251d6e991
RNT13 <renatornt13@gmail.com>
2024-12-16 15:12:04 -0300

    exercicio typescript

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = saudacao("Renato");
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = saudacao("Renato");
+
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
